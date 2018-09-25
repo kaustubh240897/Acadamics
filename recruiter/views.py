c3986fa27cf1b4c44c29775ea26cbb8bcea94a6d
unknown <kaustubh.dwi@gmail.com>
2018-09-26 04:12:42 +0530

    raising 404 error in page

--- recruiter/views.py
+++ recruiter/views.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     all_albums =Album.objects.all()
-    template=loader.get_template('recruiter/index.html')
-    context={
-        'all_albums': all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+   # template=loader.get_template('recruiter/index.html')
+    context={'all_albums': all_albums,}
+    return render(request,'recruiter/index.html',context)
 
 def detail(request,album_id):
-    return HttpResponse("<h2> Details for Album id: " +str(album_id)+ "</h2>")
+    try:
+        album=Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request,'recruiter/detail.html',{'album': album})
