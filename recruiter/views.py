3404bbf1e61b6d04f333b3434df880ce49312a91
unknown <kaustubh.dwi@gmail.com>
2018-09-26 03:32:02 +0530

    templating and fetching data from data_base

--- recruiter/views.py
+++ recruiter/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1> This is the musicapp homepage  </h1>")
+    all_albums =Album.objects.all()
+    template=loader.get_template('recruiter/index.html')
+    context={
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request,album_id):
     return HttpResponse("<h2> Details for Album id: " +str(album_id)+ "</h2>")
