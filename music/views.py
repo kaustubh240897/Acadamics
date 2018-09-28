3bdb03399e440ace881fb2e3b32549146802e623
unknown <kaustubh.dwi@gmail.com>
2018-09-29 03:05:42 +0530

    making generic views

--- music/views.py
+++ music/views.py
@@ -36,7 +36,7 @@ from .models import Album
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
-    context_object_name = 'all_albums'
+    context_object_name = 'all_album'
 
     def get_queryset(self):
         return Album.objects.all()
