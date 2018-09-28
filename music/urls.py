29baec7cc3a87e5c17b2d63bc2e23629850d7a54
unknown <kaustubh.dwi@gmail.com>
2018-09-29 03:01:38 +0530

    changing the names of directories

--- music/urls.py
+++ music/urls.py
@@ -6,13 +6,13 @@ app_name= 'music'
 
 urlpatterns = [
     #/music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     #/music/712/
-    url(r'^(?P<album_id>[0-9]+)/$' ,views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$' ,views.DetailView.as_view(), name='detail'),
 
     #/music/id/favorite
-    url(r'^(?P<album_id>[0-9]+)/favourite/$' ,views.favourite, name='favourite')
+   # url(r'^(?P<album_id>[0-9]+)/favourite/$' ,views.favourite, name='favourite')
 
 
 
--- music/views.py
+++ music/views.py
@@ -1,33 +1,78 @@
-from django.http import HttpResponse
-#from django.template import loader
-from django.http import Http404
-from django.shortcuts import render,get_object_or_404
-from .models import Album,Song
+# from django.http import HttpResponse
+# #from django.template import loader
+# from django.http import Http404
+# from django.shortcuts import render,get_object_or_404
+# from .models import Album,Song
+#
+#
+# def index(request):
+#     all_albums =Album.objects.all()
+#    # template=loader.get_template('recruiter/index.html')
+#     context={'all_albums': all_albums,}
+#     return render(request,'music/index.html',context)
+#
+# def detail(request,album_id):
+#     #album = Album.objects.get(pk=album_id)
+#     album=get_object_or_404(Album,pk=album_id)
+#     return render(request,'music/detail.html',{'album': album})
+#
+# def favourite(request, album_id):
+#     album = get_object_or_404(Album, pk=album_id)
+#     try:
+#         selected_song=album.song_set.get(pk=request.POST['song'])
+#     except(KeyError, Song.DoesNotExist):
+#         return render(request, 'music/detail.html', {
+#             'album': album,
+#             'error_message':"You did not selected valid song",
+#         })
+#     else:
+#         selected_song.is_favourite=True
+#         selected_song.save()
+#         return render(request,'music/detail.html',{'album':album})
+#
+
+from django.views import generic
+from .models import Album
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def index(request):
-    all_albums =Album.objects.all()
-   # template=loader.get_template('recruiter/index.html')
-    context={'all_albums': all_albums,}
-    return render(request,'music/index.html',context)
+class DetailView(generic.DetailView):
+    model=Album
+    template_name='music/detail.html'
 
-def detail(request,album_id):
-    #album = Album.objects.get(pk=album_id)
-    album=get_object_or_404(Album,pk=album_id)
-    return render(request,'music/detail.html',{'album': album})
 
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song=album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail.html', {
-            'album': album,
-            'error_message':"You did not selected valid song",
-        })
-    else:
-        selected_song.is_favourite=True
-        selected_song.save()
-        return render(request,'music/detail.html',{'album':album})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
