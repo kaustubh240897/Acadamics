71939b0761e83ad119eedc06fec8ad8a4410f080
unknown <kaustubh.dwi@gmail.com>
2018-09-30 06:12:43 +0530

    Adding the delete and update links

--- music/views.py
+++ music/views.py
@@ -34,6 +34,7 @@
 from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -52,6 +53,15 @@ class AlbumCreate(CreateView):
     model=Album
     fields=['artist','album_title','genre','album_logo']
     template_name = 'music/album_form.html'
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist','album_title','genre','album_logo']
+
+class AlbumDelete(DeleteView):
+    model =Album
+    success_url = reverse_lazy('music:index')
 
 
 
