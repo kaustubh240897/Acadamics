ddb741a94b2f2814f90662753d010f3ab303e834
unknown <kaustubh.dwi@gmail.com>
2018-10-01 13:09:10 +0530

    Authentication in django using forms.py

--- music/urls.py
+++ music/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     #/music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     #/music/712/
     url(r'^(?P<pk>[0-9]+)/$' ,views.DetailView.as_view(), name='detail'),
 
--- music/views.py
+++ music/views.py
@@ -33,6 +33,10 @@
 
 from django.views import generic
 from .models import Album
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
+from .forms import UserForm
 from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.core.urlresolvers import reverse_lazy
 
@@ -62,6 +66,47 @@ class AlbumUpdate(UpdateView):
 class AlbumDelete(DeleteView):
     model =Album
     success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class= UserForm
+    template_name='music/registration_form.html'
+
+    #display blank form
+    def get(self,request):
+        form=self.form_class(None)
+        return render(request,self.template_name,{'form' : form})
+
+    #process from data
+    def post(self,request):
+        form=self.form_class(request.POST)
+
+        if form.is_valid():
+
+            user=form.save(commit=False)
+
+
+            #clean and normalize data
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            #returns user objects if credentials are correct
+            user= authenticate(username=username,password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return redirect('music:index')
+
+        return render(request,self.template_name,{'form':form})
+
+
+
+
+
+
 
 
 
